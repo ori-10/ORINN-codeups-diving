bf0c9a70ceae0ceb8388e7e8bfbd14f19614b1d8
iori <09058798413iori@gmail.com>
2023-07-06 05:11:50 +0900

    header-nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,94 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+//////////////
+  // topへ戻るボタン
+  //////////////
+  var topBtn = $(".js-page-top");
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 130) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  $(document).on("click", 'a[href*="#"]', function () {
+    let time = 400;
+    let header = $(".header__inner").height();
+    let target = $(this.hash);
+    if (!target.length) return;
+    let targetY = target.offset().top - header;
+    $("html,body").animate({ scrollTop: targetY }, time, "swing");
+    return false;
+  });
+
+  //////////
+  // ハンバーガー
+  //////////
+  $(function () {
+    // ボタンクリックしたら
+    $(".js-hamburger").click(function () {
+      // アクティブクラスつけ外し
+      $(this).toggleClass("active");
+      // ナビ表示
+      $(".js-nav").toggleClass("panelactive");
+
+      // ヘッダーの色黒にする
+      $(".header").toggleClass("active");
+      // bodyに「.active」class付け外し　背景固定
+      $("body").toggleClass("active");
+    });
+
+    // リンククリックしたら
+    $(".js-nav a").click(function () {
+      // アクティブクラス外す
+      $(".js-hamburger").removeClass("active");
+      // ナビ表示
+      $(".js-nav").removeClass("panelactive");
+      // ヘッダーの色透明にする
+      $(".header").removeClass("active");
+      // bodyに「.active」class付け外し　背景固定解除
+      $("body").removeClass("active");
+    });
+  });
+
+  ////////////////
+  //   メインビュー
+  ////////////////
+
+  let swipeOption = {
+    loop: true,
+    effect: "fade",
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    speed: 2000,
+  };
+  new Swiper(".js-top-mv-swiper", swipeOption);
+
+  ////////////////
+  //   ヘッダー　mv過ぎたら黒
+  ////////////////
+
+  let header = $('.header');
+  let headerHeight = $('.header').height();
+  let height = $('.js-mv-height').height();
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > height - headerHeight) {
+       // 指定px以上のスクロールでヘッダーを表示
+      header.addClass("is-color");
+    } else {
+       // 画面が指定pxより上ならヘッダーを非表示
+      header.removeClass("is-color");
+    }
+  });
 
 
 });
