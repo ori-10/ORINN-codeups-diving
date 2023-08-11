8647327eec199f7d57776a54867df8dcdcd6c77c
iori <09058798413iori@gmail.com>
2023-08-12 03:09:50 +0900

    ファイル名変更

--- src/js/script.js
+++ src/js/script.js
@@ -310,4 +310,39 @@ jQuery(function ($) {
     $(".js-background").fadeOut(200);
     return false;
   });
+
+// コンタクト
+$(function() {
+  //始めにjQueryで送信ボタンを無効化する
+  $('.btn-submit__body').prop("disabled", true);
+  
+  //始めにjQueryで必須欄を加工する
+  $('form input:required').each(function () {
+      $(this).prev("label").addClass("required");
+  });
+  
+  //入力欄の操作時
+  $('form input:required').change(function () {
+      //必須項目が空かどうかフラグ
+      let flag = true;
+      //必須項目をひとつずつチェック
+      $('form input:required').each(function(e) {
+          //もし必須項目が空なら
+          if ($('form input:required').eq(e).val() === "") {
+              flag = false;
+          }
+      });
+      //全て埋まっていたら
+      if (flag) {
+          //送信ボタンを復活
+          $('.btn-submit__body').prop("disabled", false);
+      }
+      else {
+          //送信ボタンを閉じる
+          $('.btn-submit__body').prop("disabled", true);
+      }
+  });
+});
+
+
 }); // 消さない
